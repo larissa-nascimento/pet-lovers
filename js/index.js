8bb7f69803b89b9397371b8325117640c4f1b6c4
Larissa Nascimento <larissanascimento3061@gmail.com>
2024-10-05 10:07:20 -0300

    xereca

--- js/index.js
+++ js/index.js
@@ -3,3 +3,21 @@ document.querySelectorAll('.btn-add-cart').forEach(button => {
         alert('Produto adicionado ao carrinho!');
     });
 });
+
+
+let index = 0;
+
+function mudarImagem(direction) {
+    const imagens = document.querySelector('.imagens');
+    const totalImagens = imagens.children.length;
+
+    index += direction;
+
+    if (index >= totalImagens) {
+        index = 0;
+    } else if (index < 0) {
+        index = totalImagens - 1;
+    }
+
+    imagens.style.transform = `translateX(${-index * 100}%)`;
+}
