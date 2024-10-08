16f57ee18bb454415264ba938c58b5b86c0003f6
c3r0l <167393532+c3r0l@users.noreply.github.com>
2024-10-08 17:57:15 -0300

    .

--- Log.php
+++ Log.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style-log.css">
 </head>
 <body>
 <nav>
@@ -27,40 +28,50 @@
 
 <div class="search">
     <form action="" method="post">
-       <input type="text" name="data">
-       <button type="submit">Hello</button> 
+       <input type="text" name="data" placeholder="pesquise aqui...">
+       <button type="submit">Procurar</button> 
     </form>
     
 <?php
+
+    error_reporting(E_ERROR | E_PARSE);
     if(isset($_POST['submit'])) {
         echo "Hello world !";
     } else {
-        error_reporting(E_ERROR | E_PARSE);
+         function caralho($c, $result){
+            foreach($result as $row){
+              $login_name = $row['login'];
+              $id = $row['id'];
+              $query_time = "select time from login_time where user_id = $id;";
+              $run = $c->query($query_time);
+
+              echo "<div class='username' onclick='toggleMenu()'><h2>$login_name</h2></div>";
+
+              foreach($run as $res){
+                $time = $res['time'];
+
+                echo "
+                <ul>
+                    <li> $time</li>
+                </ul>
+                ";
+              }
+            }
+          }  
+        }
         $name = $_POST['data'];
-        $query = "select login from login where login like '%" . $name . "%';";
-    
+        $query = "select login,id from login where login like '%" . $name . "%';";
         $conn = new mysqli("localhost:3306", "root", "", "mg");
         $execution = $conn->real_query($query);
         $result = $conn->store_result();
         
-        echo "<table>";
-        foreach($result as $row){
-            $buceta = $row['login'];
-            echo "
-                <thead>
-                    <tr>
-                        <th>$buceta</th>
-                    </tr>
-                </thead>
-            ";
-            //echo "<h2>" . $row['login'] . "\n" . "</h2>";
-        }
-        echo "</table>";  
-    }
+        caralho($conn, $result);
+
+       
+       
 
     
 ?>
 </div>
-
 </body>
 </html>
