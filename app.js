0c7b86cfb378ff1fbe1343242fe69124713af829
Larissa Nascimento <larissanascimento3061@gmail.com>
2024-10-13 20:34:46 -0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const loginTimes = document.getElementById("loginTimes");
-    if (loginTimes.style.display === "none" || loginTimes.style.display === "") {
-        loginTimes.style.display = "block";
-    } else {
-        loginTimes.style.display = "none";
-    }
-}
