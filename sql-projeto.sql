4e48dfec941940d3c565ea862dd546c5335a9c33
c3r0l <167393532+c3r0l@users.noreply.github.com>
2024-10-12 21:52:25 -0300

    .

--- sql-projeto.sql
+++ sql-projeto.sql
@@ -4,17 +4,10 @@ create table produtos (
     descricao varchar(255) not null,
     valor integer not null
 
-);
+); 
 create table estoque (
 	id integer primary key not null auto_increment,
-	id_produto integer primary key not null auto_increment,
-	tipo_op varchar(50) not null,
-    id_usr varchar(100) not null,
-	horario_op timestamp not null
-);
-create table estoque (
-	id integer primary key not null auto_increment,
-	id_produto integer not null,
+	id_produto integer,
 	tipo_op varchar(50) not null,
     id_usr varchar(100) not null,
 	horario_op timestamp not null
